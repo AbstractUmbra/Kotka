2bebd9950af56e551fd14757bd93acafee8aafbf
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2023-03-16 08:02:55 +0000

    commit blank main for actions / runner purposes

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,8 @@
 
 pub mod bif;
 pub mod erf;
-mod pathfinder;
 mod shared;
-use std::{path::PathBuf, str::FromStr};
-
-use crate::bif::BIF;
-use crate::erf::Erf;
-use rfd::FileDialog;
 
 fn main() {
-    // let mut installation_path = shared::resolve_windows_registry_key()
-    //     .or(FileDialog::new().pick_folder())
-    //     .unwrap();
-
-    // println!("{:#?}", installation_path);
-    let mut installation_path = PathBuf::from_str("example_files/kotor2").unwrap();
-
-    let bif = BIF::new(&mut installation_path, None, &mut None).unwrap();
-    println!("{:#?}", bif);
-
-    // let erf = Erf::new("example_files/Game/Modules/001EBO_dlg.erf");
-
-    // println!("{:#?}", erf);
+    println!("Hello!")
 }
